da7243d64d0b34d04269607bbf8dcfff1c0db8b6
FacundoM-TUP <martinezfacundo942@gmail.com>
2025-05-22 15:50:33 -0300

    subiendo cambios

---
+++ Guia8/Ejemplo1_procedimental/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejemplo1_procedimental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region declarar variable
+            int contador, acumulador;
+            int cantidad;
+            #endregion
+
+            #region iniciar contador y acumulador
+            contador = 0;
+            acumulador = 0;
+            #endregion
+
+            #region solicitar de cantidad de ingresos
+            Console.WriteLine("Ingrese la cantidad de ingresos a procesar");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+
+        }
+    }
+}
