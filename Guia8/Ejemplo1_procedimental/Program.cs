a5344a199e7d590211b6457405bc09980932f4b6
FacundoM-TUP <martinezfacundo942@gmail.com>
2025-05-22 18:18:26 -0300

    confirmando cambios

--- Guia8/Ejemplo1_procedimental/Program.cs
+++ Guia8/Ejemplo1_procedimental/Program.cs
@@ -5,21 +5,63 @@
         static void Main(string[] args)
         {
             #region declarar variable
-            int contador, acumulador;
+            int acumulador;
             int cantidad;
+            int valor;
+            int maximo = 0, minimo = 0;
+            double promedio = 0;
             #endregion
 
             #region iniciar contador y acumulador
-            contador = 0;
             acumulador = 0;
             #endregion
 
-            #region solicitar de cantidad de ingresos
-            Console.WriteLine("Ingrese la cantidad de ingresos a procesar");
+            #region solicitar cantidad de ingresos
+            Console.WriteLine("Ingrese la cantidad de numeros a procesar");
             cantidad = Convert.ToInt32(Console.ReadLine());
             #endregion
 
+            #region iterar
+            for (int n = 0; n < cantidad; n++)
+            {
+                #region solicitar valor
+                Console.WriteLine($"ingrese el valor numero {n+1}");
+                valor = Convert.ToInt32(Console.ReadLine());
+                #endregion
 
+                #region acumular valor
+                //acumulador = acumulador + valor;
+                acumulador += valor;
+                #endregion
+
+                #region verifico valor es maximo o primer valor
+                if (valor > maximo || n == 0)
+                {
+                    maximo = valor;
+                }
+                #endregion
+
+                #region verifico valor es minimo o primer valor
+                if (valor < minimo || n == 0)
+                {
+                    minimo = valor;
+                }
+                #endregion
+            }
+            #endregion
+
+            #region calcular promedio
+            if (cantidad > 0)
+            {
+                promedio = 1d * acumulador / cantidad;
+            }
+            #endregion
+
+            #region mostrar resultados
+            Console.WriteLine($"valor maximo: {maximo}");
+            Console.WriteLine($"valor minimo: {minimo}");
+            Console.WriteLine($"promedio: {promedio}");
+            #endregion
         }
     }
 }
--- Guia8/Ejercicio1/Program.cs
+++ Guia8/Ejercicio1/Program.cs
@@ -2,6 +2,58 @@
 {
     internal class Program
     {
+        static int Acumulador;
+        static int Contador;
+        static int Maximo;
+        static int Minimo;
+
+        static void InicializarVariables()
+        {
+            Acumulador = 0;
+            Contador = 0;
+        }
+        static void RegistrarValor(int valor)
+        {
+            Acumulador = Acumulador + valor;
+            if (valor > Maximo || Contador == 0)
+            {
+
+            }
+            if (valor < Minimo || Contador == 0)
+            {
+
+            }
+
+            Contador = Contador + 1;
+        }
+           
+        static void CalcularPromedio()
+        {
+            double promedio = 0;
+        }
+
+        static void MostrarPantallaSolicitarNumero()
+        {
+
+        }
+        static void MostrarPantallaMaximoYMinimo()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Mostrar maximo y minimo. \n\n");
+
+            if (Contador > 0)
+            {
+                Console.WriteLine($"Máximo: {Maximo}");
+                Console.WriteLine($"Minimo: {Minimo}\n\n\n");
+            }
+            else
+            {
+                Console.WriteLine("no hubo ingresos todavia\n\n");
+            }
+
+            Console.WriteLine("\n\n Presione cualquier tecla para volver al menú principal");
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
